32ccfea78e74684988f27e5b33ad6e6908dc0b18
Dimitar Bogdanov <dimitar.bogdanov@yahoo.com>
2025-03-16 02:17:14 +0200

    Fix incorrect string representation

--- Ast/NodeValueWrappedCommandExecution.cs
+++ Ast/NodeValueWrappedCommandExecution.cs
@@ -5,4 +5,9 @@ namespace PercentLang.Ast;
 public sealed class NodeValueWrappedCommandExecution : NodeCommandExecution
 {
     public required Node Value { get; init; }
+
+    public override string? GetStringRepresentation(ExecutionEngine engine)
+    {
+        return Value.GetStringRepresentation(engine);
+    }
 }
